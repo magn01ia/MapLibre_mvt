137507dbee3a458abf47875687c80226b166275b
magn01ia <84883260+magn01ia@users.noreply.github.com>
2023-02-04 16:49:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,32 @@ var map = new maplibregl.Map({
     center: [140.8830781, 38.2615922],
     zoom: 13
   });
+
+map.on('load', function () {
+// Add Mapillary sequence layer.
+// https://www.mapillary.com/developer/tiles-documentation/#sequence-layer
+map.addSource('map', {
+'type': 'vector',
+'tiles': [
+'https://github.io/magn01ia/fudevectile/mito/{z}/{x}/{y}.pbf'
+],
+'minzoom': 1,
+'maxzoom': 14
+});
+map.addLayer(
+{
+'id': 'mapillary',
+'type': 'fill',
+'source': 'mito',
+'source-layer': 'sequence',
+'layout': {
+},
+'paint': {
+'line-opacity': 0.6,
+'line-color': 'rgb(53, 175, 109)',
+'line-width': 2
+}
+},
+'water_name_line'
+);
+});
